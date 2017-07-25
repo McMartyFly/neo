1a4565f2708c6613f66881b9d9e08d95a3e6d6e1
Salar Antinori <salar@Salars-MacBook-Pro.local>
2017-07-25 19:28:15 -0400

    initial css and setup

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<div>No Sleep Till Prog</div>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Soup Taste Good</title>
+    <meta name="description" value="A Tri-blend of Flavors">
+
+    <link rel="stylesheet" type="text/css" href="/css/homepage.css">
+  </head>
+
+  <body>
+    <div class="main">
+      <div class="tab-wrap">
+        <div class="tab tab-1">Resume</div>
+        <div class="tab tab-2">RAISE YOUR EYEBROW ON CENTER IMG DOG!</div>
+        <div class="tab tab-3">vertical expand on hover, shrink others, transitions</div>
+        <div class="tab tab-4">Critical Role</div>
+        <div class="tab tab-5">Tri-blend</div>
+      </div>
+      <div class="moon-bg"></div>
+    </div>
+
+  </body>
+</html>
