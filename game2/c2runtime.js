f92c36d59402dea5f57cd7ce67059d172effa04d
Salar Antinori <>
2018-11-06 17:15:43 -0500

    touch controls

--- game2/c2runtime.js
+++ game2/c2runtime.js
@@ -27049,6 +27049,80 @@ cr.behaviors.solid = function(runtime)
 	
 }());
 
+// Jump-thru
+// ECMAScript 5 strict mode
+
+;
+;
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.jumpthru = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.jumpthru.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		this.inst.extra["jumpthruEnabled"] = this.properties[0];
+	};
+
+	behinstProto.tick = function ()
+	{
+	};
+	
+	
+	function Cnds() {};
+	
+	Cnds.prototype.IsEnabled = function ()
+	{
+		return this.inst.extra["jumpthruEnabled"];
+	};
+	
+	behaviorProto.cnds = new Cnds();
+	
+	function Acts() {};
+	
+	Acts.prototype.SetEnabled = function (e)
+	{
+		this.inst.extra["jumpthruEnabled"] = !!e;
+	};
+	
+	behaviorProto.acts = new Acts();
+	
+}());
+
 cr.getObjectRefTable = function () {
 	return [
 		cr.plugins_.Sprite,
@@ -27058,8 +27132,14 @@ cr.getObjectRefTable = function () {
 		cr.plugins_.Keyboard,
 		cr.behaviors.solid,
 		cr.plugins_.Touch,
+		cr.behaviors.jumpthru,
 		cr.plugins_.Touch.prototype.cnds.IsTouchingObject,
-		cr.behaviors.Platform.prototype.acts.SimulateControl
+		cr.plugins_.Keyboard.prototype.cnds.IsKeyDown,
+		cr.plugins_.Sprite.prototype.acts.SetMirrored,
+		cr.behaviors.Platform.prototype.acts.SimulateControl,
+		cr.plugins_.Touch.prototype.cnds.OnTapGestureObject,
+		cr.plugins_.Keyboard.prototype.cnds.OnKey,
+		cr.behaviors.Platform.prototype.acts.FallThrough
 	];
 };
 
